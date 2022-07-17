9129347d1f2c393aec91ba53e497fe05b690218e
Helis Montes <helismontes@gmail.com>
2022-07-17 10:00:42 -0500

    select component restructuring

--- src/components/Select/index.js
+++ src/components/Select/index.js
@@ -1,17 +1,27 @@
+import Select from "react-select";
 import SelectStyle from "./style";
 
-const Select = ({onChange}) => {
+const frameword = [
+  { label: "Angular", value: "angular", image: "/images/angular.png" },
+  { label: "React", value: "react", image: "/images/react.png" },
+  { label: "Vue", value: "vue", image: "/images/vue.png" },
+];
+
+const SelectFrameword = ({ onChange }) => {
   return (
     <SelectStyle>
-      <select onChange={onChange}>
-        <option value="angular" select>
-          Angular
-        </option>
-        <option value="react">React</option>
-        <option value="vue">Vue</option>
-      </select>
+      <Select
+        options={frameword}
+        isSearchable={false}
+        formatOptionLabel={(frameword) => (
+          <div className="frameword-option">
+            <img src={frameword.image} alt={frameword.value} width="24px" height="24px"/>
+            <span>{frameword.label}</span>
+          </div>
+        )}
+      />
     </SelectStyle>
   );
 };
 
-export default Select
+export default SelectFrameword;
--- src/components/Select/style.js
+++ src/components/Select/style.js
@@ -2,14 +2,19 @@ import styled from "styled-components";
 
 const SelectStyle = styled.div`
   padding: 63px 150px 10px 150px;
-
-  select {
+  .css-b62m3t-container {
     width: 240px;
     height: 32px;
-    padding: 5px 12px 5px 12px;
     border-radius: 4px;
-    border: solid 1px #2e2e2e;
-    background-color: #fff;
+
+    .frameword-option {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 5px;
+      }
+    }
   }
 `;
 
