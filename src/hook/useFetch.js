3fd111883ece961a87f07cfeae80b2d412d90471
Helis Montes <helismontes@gmail.com>
2022-07-18 19:08:26 -0500

    Save my faves localstorage

--- src/hook/useFetch.js
+++ src/hook/useFetch.js
@@ -1,15 +1,13 @@
-import { useEffect, useState } from "react";
-import filterNews from "../helpers/filterNews";
-import TYPE from "../reducer/type";
-import clientAxios from "../services/clientAxios";
+import { useEffect, useState } from 'react';
+import filterNews from '../helpers/filterNews';
+import TYPE from '../reducer/type';
+import clientAxios from '../services/clientAxios';
 
 export const useFetch = (params, dispatch, state) => {
   const [loading, setLoading] = useState(false);
   const OBJECT_FRAMEWORD = state.frameword[state.selectFrameword];
-  const PAGE = parseInt(OBJECT_FRAMEWORD.page);
-  const frameword = JSON.parse(localStorage.getItem("frameword"));
   useEffect(() => {
-    if (!frameword?.[params.query].length > 0 && PAGE < 1) {
+    if (OBJECT_FRAMEWORD.news.length === 0) {
       setLoading(true);
       clientAxios
         .get(`/search_by_date`, { params })
