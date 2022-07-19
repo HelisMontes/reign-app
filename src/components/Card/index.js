5a4ff43b2d749a9e2875102ee1ab23fc1e4de75e
Helis Montes <helismontes@gmail.com>
2022-07-18 23:18:51 -0500

    Add infinite scroll

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,9 +1,20 @@
+import { useContext } from 'react';
+import { NewContext } from '../../context/NewProvaider';
+import TYPE from '../../reducer/type';
+
 import ButtomFavorite from './ButtomFavorite';
 import Description from './Description';
 
 import { CartStyle } from './style';
 
 const Cart = ({ item }) => {
+  const { state, dispatch } = useContext(NewContext);
+  function handleClick(item) {
+    dispatch({
+      type: item.faves ? TYPE.DELETE_FAVES  : TYPE.FAVES_NEWS,
+      payload: { item, library: state.selectFrameword },
+    });
+  }
   return (
     <CartStyle>
       <a target='_blank' href={item.story_url}>
@@ -13,7 +24,7 @@ const Cart = ({ item }) => {
           description={item.story_title}
         />
       </a>
-      <ButtomFavorite item={item} />
+      <ButtomFavorite item={item} onClick={() => handleClick(item)}/>
     </CartStyle>
   );
 };
